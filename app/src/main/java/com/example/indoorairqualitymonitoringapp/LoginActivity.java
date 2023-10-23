4a127599a26989831bdb40f39171e585017ac482
longpc5 <longnuyn392@gmail.com>
2023-10-24 01:25:56 +0700

    Fix Back button

--- app/src/main/java/com/example/indoorairqualitymonitoringapp/LoginActivity.java
+++ app/src/main/java/com/example/indoorairqualitymonitoringapp/LoginActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class LoginActivity extends AppCompatActivity {
-    Button back_btn;
+    ImageButton back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //back_btn = findViewById(R.id.btn_Back); //Đang gặp lỗi
+        back_btn = findViewById(R.id.btn_Back_he);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginActivity.super.onBackPressed();
+            }
+        });
     }
 }
