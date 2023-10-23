f96cef55fd2e9d9e6b310401c8389d80f01773a0
longpc5 <longnuyn392@gmail.com>
2023-10-24 00:24:10 +0700

    Code SignIn layout

--- app/src/main/java/com/example/indoorairqualitymonitoringapp/HomeActivity.java
+++ app/src/main/java/com/example/indoorairqualitymonitoringapp/HomeActivity.java
@@ -2,13 +2,30 @@ package com.example.indoorairqualitymonitoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button toSignIn_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        toSignIn_btn = findViewById(R.id.btn_SignIn);
+        toSignIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/indoorairqualitymonitoringapp/LoginActivity.java
+++ app/src/main/java/com/example/indoorairqualitymonitoringapp/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.indoorairqualitymonitoringapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //back_btn = findViewById(R.id.btn_Back); //Đang gặp lỗi
     }
 }
